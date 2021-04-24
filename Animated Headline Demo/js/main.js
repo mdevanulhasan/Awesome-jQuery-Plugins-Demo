23ad7f0ca9698534584e3df3947c6db3918e93ed
Evan <mdevanulhasan@gmail.com>
2021-04-24 21:48:42 +0600

    Animated Headline Demo Added.

---
+++ Animated Headline Demo/js/main.js
@@ -0,0 +1,42 @@
+// our custom js code is given below.
+$(function() {
+	$('.animate-rotate-1').animatedHeadline({
+		animationType: 'rotate-1'
+	});
+
+	$('.animate-type').animatedHeadline({
+		animationType: 'type'
+	});
+
+	$('.animate-rotate-2').animatedHeadline({
+		animationType: 'rotate-2'
+	});
+
+	$('.animate-loading-bar').animatedHeadline({
+		animationType: 'loading-bar'
+	});
+
+	$('.animate-slide').animatedHeadline({
+		animationType: 'slide'
+	});
+
+	$('.animate-clip').animatedHeadline({
+		animationType: 'clip'
+	});
+
+	$('.animate-zoom').animatedHeadline({
+		animationType: 'zoom'
+	});
+
+	$('.animate-rotate-3').animatedHeadline({
+		animationType: 'rotate-3'
+	});
+
+	$('.animate-scale').animatedHeadline({
+		animationType: 'scale'
+	});
+
+	$('.animate-push').animatedHeadline({
+		animationType: 'push'
+	});
+});
