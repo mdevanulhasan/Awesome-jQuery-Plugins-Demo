d87289e97c16554120d75bd41023facbd42088f1
mdevanulhasan <mdevanulhasan@gmail.com>
2021-06-04 22:58:20 +0600

    Preloader Demo Added.

---
+++ Preloader Demo/js/main.js
@@ -0,0 +1,43 @@
+// Our Custom Javascript or jQuery Codes is given below.
+(function ($) {
+	var preloader1 = document.getElementById("preloader1");
+	window.addEventListener("load", function(){
+    preloader1.style.display = "none";
+	});
+	var preloader2 = document.getElementById("preloader2");
+	window.addEventListener("load", function(){
+    preloader2.style.display = "none";
+	});
+	var preloader3 = document.getElementById("preloader3");
+	window.addEventListener("load", function(){
+    preloader3.style.display = "none";
+	});
+	var preloader4 = document.getElementById("preloader4");
+	window.addEventListener("load", function(){
+    preloader4.style.display = "none";
+	});
+	var preloader5 = document.getElementById("preloader5");
+	window.addEventListener("load", function(){
+    preloader5.style.display = "none";
+	});
+	var preloader6 = document.getElementById("preloader6");
+	window.addEventListener("load", function(){
+    preloader6.style.display = "none";
+	});
+	var preloader7 = document.getElementById("preloader7");
+	window.addEventListener("load", function(){
+    preloader7.style.display = "none";
+	});
+	var preloader8 = document.getElementById("preloader8");
+	window.addEventListener("load", function(){
+    preloader8.style.display = "none";
+	});
+	var preloader9 = document.getElementById("preloader9");
+	window.addEventListener("load", function(){
+    preloader9.style.display = "none";
+	});
+	var preloader10 = document.getElementById("preloader10");
+	window.addEventListener("load", function(){
+    preloader10.style.display = "none";
+	});
+})(jQuery);
