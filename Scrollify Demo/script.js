6538de2b7d9cf7564e93faaf55e270affca32db6
Md Evanul Hasan <mdevanulhasan@gmail.com>
2021-08-13 00:27:23 +0600

    Scrollify Demo Added.

---
+++ Scrollify Demo/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	// Scrollify
+	$.scrollify({
+		section: ".scrollify",
+		setHeights: false,
+		updateHash: false,
+		scrollSpeed: 1100,
+		offset : 0,
+		scrollbars: false,
+		overflowScroll: false,
+		touchScroll: false,
+	});
+});
